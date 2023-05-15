c0a3c20ca734e8691efb4bea5b02775f627e2abb
Xiyao Zhao <xiyao.zhao28@gmail.com>
2023-05-14 21:15:03 -0400

    add NO_ERRORS_SCHEMA

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
 import { Hero } from "../hero";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 describe("HeroComponnet (shallow tests)", () => {
   let fixture: ComponentFixture<HeroComponent>;
@@ -8,13 +9,13 @@ describe("HeroComponnet (shallow tests)", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     });
     fixture = TestBed.createComponent(HeroComponent);
   });
 
   it("should have the correct hero", () => {
     fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3 };
-    fixture.detectChanges();
 
     expect(fixture.componentInstance.hero.name).toEqual("SuperDude");
   });
