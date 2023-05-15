cfd48b4041b9b5461ace16345d8aa01777d09e94
Xiyao Zhao <xiyao.zhao28@gmail.com>
2023-05-14 21:27:36 -0400

    add an integration test to test rendered HTML

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -19,4 +19,13 @@ describe("HeroComponnet (shallow tests)", () => {
 
     expect(fixture.componentInstance.hero.name).toEqual("SuperDude");
   });
+
+  it("should render the hero name in an anchor tag", () => {
+    fixture.componentInstance.hero = { id: 1, name: "SuperDude", strength: 3 };
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector("a").textContent).toContain(
+      "SuperDude"
+    );
+  });
 });
