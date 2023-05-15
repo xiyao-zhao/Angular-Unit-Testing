fb01994c5ff538b5d4c4d1013389d59008d3520e
Xiyao Zhao <xiyao.zhao28@gmail.com>
2023-05-14 22:46:08 -0400

    mock child component

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroesComponent } from "./heroes.component";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { HeroService } from "../hero.service";
 import { of } from "rxjs";
+import { Hero } from "../hero";
 
 describe("HeroesComponent (shallow tests)", () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  @Component({
+    selector: "app-hero",
+    template: "<div></div>",
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
 
   beforeEach(() => {
     HEROES = [
@@ -22,9 +32,9 @@ describe("HeroesComponent (shallow tests)", () => {
     ]);
 
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, FakeHeroComponent],
       providers: [{ provide: HeroService, useValue: mockHeroService }],
-      schemas: [NO_ERRORS_SCHEMA],
+      //   schemas: [NO_ERRORS_SCHEMA],
     });
     fixture = TestBed.createComponent(HeroesComponent);
   });
